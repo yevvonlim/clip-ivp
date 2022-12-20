cb1d209fbc80f0fc0dcf125114e4d1ca62870aee
Yewon <ga06033@gmail.com>
2022-12-20 07:35:34 +0000

    221220 | plot updated

--- plot_results.py
+++ plot_results.py
@@ -9,14 +9,17 @@ plt.ylabel('FID')
 plt.xlabel('Iterations')
 
 root_dir = '/root/stylegan2-ada-intraoral/class-conditional/'
-keys = ['gamma5', 'gamma15']
-progress_path = {key:f'{root_dir}cond-auto1-{key}-resumecustom/metric-fid50k_full.jsonl' for key in keys}
+keys = ['gamma5', 'gamma10', 'gamma15']
+progress_path = {key:f'{root_dir}cond-auto1-{key}-resumecustom/metric-fid50k_full.jsonl' for key in keys if key != 'gamma10'}
+progress_path['gamma10'] = '/root/stylegan2-ada-intraoral/class-conditional/gamma10/metric-fid50k_full.jsonl'
 progress = {}
 for key in keys:
     fid = []
     steps = []
     with jsonlines.open(progress_path[key]) as f:
         for line in f:
+            if int(line['snapshot_pkl'].replace('network-snapshot-', '').replace('.pkl', '')) > 15000:
+                break
             fid.append(line['results']['fid50k_full'])
             steps.append(int(line['snapshot_pkl'].replace('network-snapshot-', '').replace('.pkl', '')))
     progress[key] = list(zip(steps, fid))
@@ -34,12 +37,15 @@ def inverse(x):
 
 # plt.xticks([0, 1000, 2000, 4000, 8000, 15000])
 steps_ = [0,  1000, 2000, 4000, 8000, 12000, 15000]
-labels_ = [f'{int(step/1000)}k' if step != 0 and step%1000==0 else f'{step/1000}k' if step%1000 != 0 else '0' for step in steps_]
+labels_ = [f'{int(step/1000)}M' if step != 0 and step%1000==0 else f'{step/1000}k' if step%1000 != 0 else '0' for step in steps_]
 
 # plt.xscale('function', functions=(forward, inverse))
 plt.xscale('linear')
+plt.yscale('linear')
+plt.yticks([3, 4, 5, 6, 15, 30])
 plt.xticks(steps_, labels=labels_)
-plt.ylim(top=30)
+plt.ylim(top=20, bottom=1)
+plt.xlim(left=0)
 # plt.yscale('function', functions=(forward, inverse))
 ax.legend()
-plt.savefig('test.png')
+plt.savefig('/root/stylegan2-ada-intraoral/gamma_sweep.png')
